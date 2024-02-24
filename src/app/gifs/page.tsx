e9ed7912ac0dd5e0668d5bb138edb697a76305ae
BluubiDev <bluubi.dev@gmail.com>
2024-02-24 23:12:37 +0100

    first commit

---
+++ src/app/gifs/page.tsx
@@ -0,0 +1,5 @@
+
+const giphy = 'https://api.giphy.com/v1/gifs/trending?api_key=8pyHN7FvL8QLGvPP22arDUrB1LGfjV3T'
+export default function Giphy(){
+    return <h2> Giphyri </h2>
+}
---
+++ src/app/gifs/trending-gifs/page.tsx
@@ -0,0 +1,3 @@
+export default function TrendingGiphs(){
+    return (<h1> Trending giphs </h1>)
+}
